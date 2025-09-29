a45b50eacaa30462d71b8f341d32ba86e65e290a
Wander Nobel <w.a.nobel@student.tudelft.nl>
2025-09-29 16:57:51 +0200

    added step function

--- signals.py
+++ signals.py
@@ -1,1 +1,11 @@
-generate_sine_wave(frequency, duration, sample_rate) 
+import numpy as np 
+
+def generate_sine_wave(frequency, duration, sample_rate):
+    samples = duration * sample_rate 
+    t = np.linspace(0, duration, samples)
+    wave = np.sin(2 * np.pi * frequency * t)
+    return wave 
+
+def generate_step_function(t, step_time)
+    return np.where(t < step_time, 0, 1)
+
