c66e3bba3b233a6fe03b8fd4b2d8c76b3f2686b9
Wander Nobel <w.a.nobel@student.tudelft.nl>
2025-09-29 16:38:01 +0200

    updated sine plot

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 
 from signals import generate_sine_wave
 wave = generate_sine_wave(5, 2, 100)
-
-#print(wave[:10])
 plt.plot(wave)
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.title('Sinusoidal Signal')
 plt.show()
