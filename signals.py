5a92c6e3e0ce72f386c16379968ffa9af4992bf9
Wander Nobel <w.a.nobel@student.tudelft.nl>
2025-09-29 18:13:48 +0200

    changed the sine function

--- signals.py
+++ signals.py
@@ -1,11 +1,11 @@
 import numpy as np 
 
-def generate_sine_wave(frequency, duration, sample_rate):
-    samples = duration * sample_rate 
-    t = np.linspace(0, duration, samples)
-    wave = np.sin(2 * np.pi * frequency * t)
-    return wave 
+def generate_sine_wave(frequency, duration, time_shift):
+    t = np.linspace(0, duration, 100)
+    wave = np.sin(2 * np.pi * frequency * t + time_shift)
+    return t, wave 
 
 def generate_step_function(t, step_time):
     return np.where(t < step_time, 0, 1)
+
 
