543b950945c82d5d15c06c30d135d85a3d630998
Wander Nobel <w.a.nobel@student.tudelft.nl>
2025-09-29 17:08:56 +0200

    corrected some errors

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 from signals import generate_sine_wave
@@ -8,7 +9,7 @@ plt.ylabel('Amplitude')
 plt.title('Sinusoidal Signal')
 plt.show()
 
-from signals import generate_step_function(t, step_time)
+from signals import generate_step_function
 t = np.linspace(-1,1)
 step = generate_step_function(t, 0)
 plt.plot(t, step)
