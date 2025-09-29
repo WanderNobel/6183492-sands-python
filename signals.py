08ae2dc93b5c7a034c16eba485a9279f8aaf322b
Wander Nobel <w.a.nobel@student.tudelft.nl>
2025-09-29 18:20:24 +0200

    Added the time-scaling element to the sine function

--- signals.py
+++ signals.py
@@ -1,8 +1,8 @@
 import numpy as np 
 
-def generate_sine_wave(frequency, duration, time_shift):
+def generate_sine_wave(frequency, duration, time_shift, time_scale):
     t = np.linspace(0, duration, 100)
-    wave = np.sin(2 * np.pi * frequency * t + time_shift)
+    wave = np.sin(2 * np.pi * frequency * t * time_scale + time_shift)
     return t, wave 
 
 def generate_step_function(t, step_time):
