024d945e55eda2dfde9f19202078b5c7c9c88902
Wander Nobel <w.a.nobel@student.tudelft.nl>
2025-10-23 08:40:51 +0200

    toml file with metadeta

---
+++ .ipynb_checkpoints/run-checkpoint.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from signals import generate_sine_wave
+t, wave = generate_sine_wave(1, 1, 0, 1)
+plt.figure(1)
+plt.plot(t, wave)
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.title('Sinusoidal Signal')
+plt.show()
+
+from signals import generate_step_function
+t = np.linspace(-1,1)
+step = generate_step_function(t, 0)
+plt.figure(2)
+plt.plot(t, step)
+plt.ylabel('Amplitude')
+plt.xlabel('Time')
+plt.title('Step Function')
+plt.show()
+
+from signals import generate_sine_wave
+t, wave = generate_sine_wave(1, 1, 0.5, 1)
+plt.figure(3)
+plt.plot(t, wave)
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.title('Time-Shifted Sinusoidal Signal')
+plt.show()
+
+from signals import generate_sine_wave
+t, wave = generate_sine_wave(1, 1, 0.5, 2)
+plt.figure(4)
+plt.plot(t, wave)
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.title('Time-Shifted and Time-Scaled Sinusoidal Signal')
+plt.show()
